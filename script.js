b2e1131db9d0fae3f82b04fd2e5eb284196ad207
dselimovic <dselimovic02@gmail.com>
2023-12-09 00:53:48 +0100

    Write README

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let questions = document.querySelectorAll(".question");
 
 questions.forEach(question => {
     let img = question.querySelector("img");
-    question.addEventListener("click", () =>{
-        if(img.src.includes("minus"))
+    question.addEventListener("click", () => {
+        if (img.src.includes("minus"))
             img.src = "assets/images/icon-plus.svg";
         else
             img.src = "assets/images/icon-minus.svg";
