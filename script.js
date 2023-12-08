f5f3d42a54c3c9e4470f1142c1b6ed9891570967
dselimovic <dselimovic02@gmail.com>
2023-12-09 00:26:31 +0100

    Change icon on open and close with js script

---
+++ script.js
@@ -0,0 +1,11 @@
+let questions = document.querySelectorAll(".question");
+
+questions.forEach(question => {
+    let img = question.querySelector("img");
+    question.addEventListener("click", () =>{
+        if(img.src.includes("minus"))
+            img.src = "assets/images/icon-plus.svg";
+        else
+            img.src = "assets/images/icon-minus.svg";
+    });
+});
